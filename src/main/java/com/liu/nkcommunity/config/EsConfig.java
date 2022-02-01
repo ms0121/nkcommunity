905a6e5debda375e24feeef49d40de39fbafca05
liumingshan3 <liumingshan3@jd.com>
2022-02-01 22:09:37 +0800

    修改bug、并整合全文搜索引擎ElasticSearch

--- src/main/java/com/liu/nkcommunity/config/EsConfig.java
+++ src/main/java/com/liu/nkcommunity/config/EsConfig.java
@@ -4,10 +4,10 @@ package com.liu.nkcommunity.config;
 public class EsConfig {
 
     // 创建连接
-//    @Bean
-//    public RestHighLevelClient restHighLevelClient() {
-//        return new RestHighLevelClient(RestClient.builder(new HttpHost("81.69.27.163", 9200, "http")));
-//    }
+    //    @Bean
+    //    public RestHighLevelClient restHighLevelClient() {
+    //        return new RestHighLevelClient(RestClient.builder(new HttpHost("81.69.27.163", 9200, "http")));
+    //    }
 
 
 }
